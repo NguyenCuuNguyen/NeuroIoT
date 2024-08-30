910f68abaf79020910b8d4a65fcdde10706a0d45
NguyenCuuNguyen <nguyen2nk@gmail.com>
2024-08-29 23:59:12 -0600

    export images

--- preprocess/video_analysis.py
+++ preprocess/video_analysis.py
@@ -1,8 +1,9 @@
 import cv2
+import os
 
-vid_path = '/Users/irisnguyen/Documents/Utah PhD/Neurosymbolic Research/Code/input/gopro/GH010069.MP4'
-num_frames = 10
-
+vid_path = '/Users/irisnguyen/Documents/Utah PhD/Neurosymbolic Research/Code/IO/gopro/GH010069.MP4'
+num_frames = 100
+imgs_path = '/Users/irisnguyen/Documents/Utah PhD/Neurosymbolic Research/Code/IO/imagesOutput/NaieveSamples'
 
 def sample_frames(vid, num_frames):
     total_frames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -25,6 +26,14 @@ def sample_frames(vid, num_frames):
 
     return frames
 
+def save_imgs(folder_path, image_array):
+    for i in range(len(image_array)):
+        img = cv2.cvtColor(image_array[i], cv2.COLOR_RGB2BGR)
+
+        file_name = "sample-"+str(i)+".jpg"
+        file_path = os.path.join(folder_path, file_name)
+        cv2.imwrite(file_path, img)
+
 if __name__ == "__main__":
 # Check if the video opened successfully
     cap = cv2.VideoCapture(vid_path)
@@ -34,11 +43,13 @@ if __name__ == "__main__":
 
     sampled_frames = sample_frames(cap, num_frames)
     
+    save_imgs(imgs_path, sampled_frames)
     # Release the video capture object
     cap.release()
 
     
-    for i, frame in enumerate(sampled_frames):
-        cv2.imshow(f"Frame {i}", frame)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows() # Close all OpenCV windows
+    # for i, frame in enumerate(sampled_frames):
+    #     print(type(frame))
+    #     cv2.imshow(f"Frame {i}", frame)
+    #     cv2.waitKey(0)
+    cv2.destroyAllWindows() # Close all OpenCV windows
